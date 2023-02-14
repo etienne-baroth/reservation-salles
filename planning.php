1c345b71827751c1065e0989e0d6387d331f9380
etienne-baroth <etienne.baroth@laplateforme.io>
2023-02-14 10:12:18 +0100

    modified variable names

--- planning.php
+++ planning.php
@@ -38,18 +38,18 @@ $info_resa = $requete_resa->fetchALL(PDO::FETCH_ASSOC);
 
 <div>
 
-    <h1>Planning <?php echo $jour_semaine = date('Y', time()); ?></h1>
-    <h2>Semaine <?php echo $jour_semaine = date('W', time()); ?></h2>
+    <h1>Planning <?php echo $day_week = date('Y', time()); ?></h1>
+    <h2>Semaine <?php echo $day_week = date('W', time()); ?></h2>
             
         <table>
                 <thead>
                     <tr>
                         <th></th>
-                        <th>Lundi <?php echo $jour_semaine = date('d/m', strtotime('monday this week')); ?></th>
-                        <th>Mardi <?php echo $jour_semaine = date('d/m', strtotime('tuesday this week')); ?></th>
-                        <th>Mercredi <?php echo $jour_semaine = date('d/m', strtotime('wednesday this week')); ?></th>
-                        <th>Jeudi <?php echo $jour_semaine = date('d/m', strtotime('thursday this week')); ?></th>
-                        <th>Vendredi <?php echo $jour_semaine = date('d/m', strtotime('friday this week')); ?></th>
+                        <th>Lundi <?php echo $day_week = date('d/m', strtotime('monday this week')); ?></th>
+                        <th>Mardi <?php echo $day_week = date('d/m', strtotime('tuesday this week')); ?></th>
+                        <th>Mercredi <?php echo $day_week = date('d/m', strtotime('wednesday this week')); ?></th>
+                        <th>Jeudi <?php echo $day_week = date('d/m', strtotime('thursday this week')); ?></th>
+                        <th>Vendredi <?php echo $day_week = date('d/m', strtotime('friday this week')); ?></th>
                     </tr>
                 </thead>
 
@@ -63,7 +63,7 @@ $info_resa = $requete_resa->fetchALL(PDO::FETCH_ASSOC);
                                 <p><?php echo $heure . "h"; ?></p>
                             </td>
                             <?php
-                            for ($jour = 1; $jour <= 5; $jour++)
+                            for ($day = 1; $day <= 5; $day++)
                         {
 
                                 if (!empty($info_resa)) {
@@ -75,10 +75,10 @@ $info_resa = $requete_resa->fetchALL(PDO::FETCH_ASSOC);
                                         $heure_resa = date("G", mktime($H[0], $H[1], $H[2], 0, 0, 0));
 
                                         $J = explode("-", $JH[0]);
-                                        $jour_resa = date("N", mktime(0, 0, 0, $J[1], $J[2], $J[0]));
+                                        $day_resa = date("N", mktime(0, 0, 0, $J[1], $J[2], $J[0]));
 
-                                        $case_resa = $heure_resa . $jour_resa;
-                                        $case = $heure . $jour;
+                                        $case_resa = $heure_resa . $day_resa;
+                                        $case = $heure . $day;
 
 
                                         $titre = $Hresa["titre"];
@@ -103,12 +103,12 @@ $info_resa = $requete_resa->fetchALL(PDO::FETCH_ASSOC);
                                     }
                                     if ($case == null) {
                                         ?>
-                                        <td><a href="reservation-form.php?heure_debut=<?php echo $heure; ?>&amp;date_debut=<?php echo $jour; ?>">Réserver l'horaire</a></td>
+                                        <td><a href="reservation-form.php?heure_debut=<?php echo $heure; ?>&amp;date_debut=<?php echo $day; ?>">Réserver l'horaire</a></td>
                                     <?php
                                     }
                                 } else {
                                     ?>
-                                    <td><a href="reservation-form.php?heure_debut=<?php echo $heure; ?>&amp;date_debut=<?php echo $jour; ?>">Réserver l'horaire</a></td>
+                                    <td><a href="reservation-form.php?heure_debut=<?php echo $heure; ?>&amp;date_debut=<?php echo $day; ?>">Réserver l'horaire</a></td>
                                 <?php
                                 }
                             } ?>
