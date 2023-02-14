94154451900fa38610b853cc594c7fb2f139103c
etienne-baroth <etienne.baroth@laplateforme.io>
2023-02-14 10:42:34 +0100

    added favicon

--- reservation.php
+++ reservation.php
@@ -42,6 +42,7 @@ if (isset($_GET["evenement"]) && !empty($_GET["evenement"])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="ressources/favicon.png">
 </head>
 <body>
 
